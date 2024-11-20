a6ec2498831806f8fce040a878fb567404f48bcf
F1Soda <golik.tima@gmail.com>
2024-11-20 23:35:40 +0500

    Before Merge with Artem branch

--- FiveMinute/.vshistory/Program.cs/2024-11-19_20_04_26_802.cs
+++ FiveMinute/.vshistory/Program.cs/2024-11-19_20_04_26_802.cs
@@ -38,13 +38,13 @@ app.ApplyMigrations();
 await Seed.SeedUsersDefailt(app);
 
 // Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 
-//}
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
