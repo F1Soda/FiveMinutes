6c2fc4a97fec8b1ef303ab9e6fa77a74efa77db4
F1Soda <golik.tima@gmail.com>
2024-12-06 10:54:46 +0500

    Tranfer table with data about tests and templates to main page

--- FiveMinute/Controllers/HomeController.cs
+++ FiveMinute/Controllers/HomeController.cs
@@ -1,24 +1,51 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using FiveMinute.Models;
+using FiveMinute.ViewModels.AccountViewModels;
+using FiveMinute.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Globalization;
 
 namespace FiveMinute.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
-        private readonly UserManager<AppUser> userManager;
+		private readonly ApplicationDbContext context;
+		private readonly UserManager<AppUser> userManager;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ApplicationDbContext context, UserManager<AppUser> userManager)
 		{
-			_logger = logger;
+			this.context = context;
 			this.userManager = userManager;
 		}
 
 		public async Task<IActionResult> Index()
 		{
-			return View(userManager);
+			var currentUser = await userManager.GetUserAsync(User);
+
+
+			UserDetailViewModel model = null!;
+			if (currentUser != null)
+			{
+				// Тут возможно двойная работа, но пускай так будет
+				var user = await context.Users.Include(x => x.FMTTemplates)
+					.Include(x => x.FMTests)
+					.FirstOrDefaultAsync(x => x.Id == currentUser.Id);
+
+				model = new UserDetailViewModel
+				{
+					UserName = user.UserName,
+					Email = user.Email,
+					FMTs = user.FMTTemplates,
+					Tests = user.FMTests,
+					UserRole = currentUser.UserRole,
+					IsOwner = true,
+
+				};
+
+			}
+			return View(model);
 		}
 
 		public IActionResult Privacy()
