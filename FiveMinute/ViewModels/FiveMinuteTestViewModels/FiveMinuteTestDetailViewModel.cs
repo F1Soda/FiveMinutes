68d23ff0c8a7d645ff8476a80ff77f965a44fa0d
boboboba <trascinsavelij@gmail.com>
2024-12-19 21:53:18 +0500

    fixed date

--- FiveMinute/ViewModels/FiveMinuteTestViewModels/FiveMinuteTestDetailViewModel.cs
+++ FiveMinute/ViewModels/FiveMinuteTestViewModels/FiveMinuteTestDetailViewModel.cs
@@ -1,5 +1,7 @@
-﻿using FiveMinute.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using FiveMinute.Models;
 using FiveMinute.Data;
+using FiveMinute.ViewModels.FiveMinuteTestViewModels;
 using FiveMinute.ViewModels.FMTEditViewModels;
 using FiveMinute.ViewModels.Interfaces;
 
@@ -14,9 +16,12 @@ namespace FiveMinute.ViewModels.FiveMinuteTestViewModels
 	public TestStatus Status { get; set; }
 
 	public bool StartPlanned { get; set; }
+	
 	public DateTime StartTime { get; set; }
 	public bool EndPlanned  { get; set; }
 	public DateTime EndTime { get; set; }
+	[DateRange(ErrorMessage = "Дата начала должна быть меньше даты конца.")]
+	public bool IsValidDateRange => StartTime < EndTime;
 	public IEnumerable<FiveMinuteTestResult> Results { get; set; }
 	public List<int> IdToUninclude { get; set; }
 
@@ -44,12 +49,27 @@ namespace FiveMinute.ViewModels.FiveMinuteTestViewModels
 			Id = FMTestView.Id,
 			Name = FMTestView.Name,
 			StartPlanned = FMTestView.StartPlanned,
-			StartTime = FMTestView.StartTime,
+			StartTime = FMTestView.StartTime.ToUniversalTime(),
 			EndPlanned = FMTestView.EndPlanned,
-			EndTime = FMTestView.EndTime,
+			EndTime = FMTestView.EndTime.ToUniversalTime(),
 			Status = FMTestView.Status,
 			IdToUninclude = FMTestView.IdToUninclude,
 		};
 	}
+	}
+}
+
+public class DateRangeAttribute : ValidationAttribute
+{
+	protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+	{
+		var model = (FiveMinuteTestDetailViewModel)validationContext.ObjectInstance;
+
+		if (model.StartTime >= model.EndTime)
+		{
+			return new ValidationResult(ErrorMessage);
+		}
+
+		return ValidationResult.Success;
 	}
 }
